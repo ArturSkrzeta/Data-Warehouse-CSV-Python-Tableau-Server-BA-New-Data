f769f49423693665c97ed115ceb6a2c785f6759e
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2021-06-16 22:14:20 +0200

    Add files via upload

--- project/etl_for_excels.py
+++ project/etl_for_excels.py
@@ -10,6 +10,8 @@ def get_flat_files():
     for dir, subdirs, files in os.walk(path):
         for file in files:
             if file.endswith(".xlsx"):
+                if file.find("combined") > - 1:
+                    continue
                 flat_files.append(f'{dir}{os.sep}{file}')
     return flat_files
 
@@ -18,20 +20,26 @@ def read_xlsx_to_df(file):
     file_name = left(file_name_with_extension, len(file_name_with_extension) - 5)
     xlsx = pd.ExcelFile(file)
     df = pd.read_excel(xlsx, 'Closed items')
+    print(file_name)
+    print(df.shape[0])
     df['tracker_name'] = file_name
     return df
 
 def main():
 
+    # extract
     files = get_flat_files()
-    count = len(files)
     dfs = [read_xlsx_to_df(file) for file in files]
+
+    # transform
     combined = pd.concat(dfs)
-    combined.to_csv('combined.csv', index=False)
+    combined['Group Company'] = combined['Group Company'].map(lambda x: 'LHT' if x in ['LGB', 'LGM', 'LGE'] else x)
+    combined.loc[(combined['Detailed Status'] == '5 - PO - sent to supplier') & (combined['Banf quality'].isnull()), 'Banf quality'] = 'not evaluated'
+    combined.loc[(combined['Detailed Status'] == '7 - PR - rerouted') & (combined['Banf quality'].isnull()), 'Banf quality'] = 'rerouted'
+    combined['PR nr'] = combined['PR nr'].astype(str)
+
+    # load
+    combined.to_excel('combined.xlsx', index=False)
 
 if __name__ == "__main__":
     main()
-
-# xls = pd.ExcelFile('path_to_file.xls')
-# df1 = pd.read_excel(xls, 'Sheet1')
-# df2 = pd.read_excel(xls, 'Sheet2')
